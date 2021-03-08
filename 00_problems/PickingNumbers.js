5839cf1b67ceba6dfae8a9caadbb768a9606c858
ROBIN OCHOA <robin8a@local.mac>
2021-03-08 15:28:42 -0500

    update

--- 00_problems/PickingNumbers.js
+++ 00_problems/PickingNumbers.js
@@ -7,23 +7,50 @@ function PickingNumber(arr) {
     var count = 0
 
     arr.sort(function(a, b){return a - b});
+    var lastIndexMatch = -1
 
-    for (var i=0; i < arr.length; i++) {
-        var actualNumber = arr[i]
+    while (lastIndexMatch < arr.length-1 ) {
+
+        if (lastIndexMatch === 0) {
+            var actualNumber = arr[lastIndexMatch]
+        }else {
+            var actualNumber = arr[lastIndexMatch+1]
+        }
+
         var countNumber = 0
-        for (var j=i+1; j < arr.length; j++) {
+        for (var j=lastIndexMatch+1; j < arr.length; j++) {
             if (actualNumber === arr[j]) {
+                lastIndexMatch = j
                 countNumber++
             }
         }
         frecuencia.push(countNumber)
     }
 
-    i       j       actualNumber    countNumber     frecuencia
-    0       1       1               0               []
-    0       1       1               1               []
-    0       2       1               2
+    return frecuencia
+
+    // i       j       actualNumber    countNumber     frecuencia      lastIndexMatch
+    // 0       0       1               1               []                  0
+    // 0       1       1               2               []                  1
+    // 0       2       2               2               []                  1
+    // 0       3       2               2               [] ...              1
+    // 0       7       2               2               [2]                 1
+    // 1       2       2               1               [2]                 2
+    // 1       3       2               2               [2]                 3
+    // 1       7       2               2               [2,2]...            3
+    // 2       4       4               1               [2,2]               4
+    // 2       5       4               2               [2,2]               5
+    // 2       6       4               2               [2,2]               5
+    // 2       7       4               2               [2,2,2]             5
+    //         6       5               1               [2,2,2]             6
+    //         7       5               2               [2,2,2]             7
+    //         8       5               3               [2,2,2]             8
+    //         8       5               3               [2,2,2,3]           8 
+
+
+
     
+
     // [1,1,2,2,4,4,5,5,5]
     // [2,2,2,3]
     // 2+2 =4
@@ -47,5 +74,25 @@ function PickingNumber(arr) {
     // max = 2 + 3 = 5
     // 3 + 1 = 4
     // max = 5
+
+}
+
+
+var arrFrecuencia1 = PickingNumber([1,1,2,2,4,4,5,5,5])
+console.log('Freq array1: ', arrFrecuencia1)
+
+
+var arrFrecuencia2 = PickingNumber([1,2,2,3,1,2])
+console.log('Freq array2: ', arrFrecuencia2)
+
+
+
+
+function PickingNumberOptionTwo(arr) { 
+    arr.sort(function(a, b){return a - b})
+
+    // 1. Encontrar los sub-arreglos que cumplan con los criterios
+    // 2. Encontar el mayor de esos arreglo y retornar el valor
+    
 
 }
